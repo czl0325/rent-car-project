29538c7013cbd808f135a38e369e16a7bf025659
陈昭良 <czl2106082@163.com>
2021-11-18 11:12:11 +0800

    grpc gateway启动

--- service/main.go
+++ service/main.go
@@ -1,19 +1,40 @@
 package main
 
 import (
+	"context"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"google.golang.org/grpc"
 	"log"
 	"net"
+	"net/http"
 	trippb "service/proto/gen/go"
 	trip "service/tripService"
 )
 
 func main() {
-	listener, err := net.Listen("tcp", ":8081")
+	log.SetFlags(log.Lshortfile)
+	go startGRPCGateway()
+	listener, err := net.Listen("tcp", ":9081")
 	if err != nil {
 		log.Fatalf("服务器启动失败，错误=%v", err)
 	}
 	s := grpc.NewServer()
 	trippb.RegisterTripServiceServer(s, &trip.Service{})
-	s.Serve(listener)
+	log.Fatalln(s.Serve(listener))
+}
+
+
+func startGRPCGateway() {
+	c := context.Background()
+	c, cancel := context.WithCancel(c)
+	defer cancel()
+	mux := runtime.NewServeMux()
+	err := trippb.RegisterTripServiceHandlerFromEndpoint(c, mux, ":9081", []grpc.DialOption{grpc.WithInsecure()})
+	if err != nil {
+		log.Fatalf("启动grpc gateway失败。错误=%v\n", err)
+	}
+	err = http.ListenAndServe(":9000", mux)
+	if err != nil {
+		log.Fatalf("启动http服务失败。错误=%v\n", err)
+	}
 }
